3c33e1f5af4e9c8fc2c5f2f1baac75fde6343b7d
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2019-02-02 15:14:16 +0200

    Startup Code

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Landing Page
+app.get("/", (req, res) => {
+    res.render("home", {title: "Home"});
+});
+
+//Campgrounds Page
+app.get("/campgrounds", (req, res) => {
+    var camps = [
+        {name : "Salmon Creek", url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtiiax3c_G7gA7er8Il2ykfVLqX36bGuntLpWBhVw8NCrc9Ckr"},
+        {name : "Granite Hill", url : "https://www.nationalparks.nsw.gov.au/-/media/npws/images/parks/munmorah-state-conservation-area/background/freemans-campground-background.jpg"},
+        {name : "Mountain Goat's Rest", url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK6OAkMx02Vgb-inp1RwSWA6yqsWXGvPC-hqrZdmpQQx6cRgqs"},
+    ];
+    res.render("campgrounds" , {title: "campgrounds", camps : camps});
+});
+
+//Listening
+app.listen(3000, () => {
+    console.log("Listening at port 3000 ...");
+});
